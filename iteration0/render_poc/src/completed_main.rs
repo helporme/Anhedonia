aa6981eda08a312652880b1dfefaee25910bd251
Daniel <helporme.dev@gmail.com>
2021-12-11 16:46:25 +0200

    https://sotrh.github.io/learn-wgpu "The Pipeline" completed

--- iteration0/render_poc/src/completed_main.rs
+++ iteration0/render_poc/src/completed_main.rs
@@ -9,9 +9,7 @@ struct State {
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
-    size: winit::dpi::PhysicalSize<u32>,
-    clear_color: wgpu::Color,
-    render_pipeline: wgpu::RenderPipeline,
+    size: winit::dpi::PhysicalSize<u32>
 }
 
 impl State {
@@ -51,55 +49,6 @@ impl State {
         };
 
         surface.configure(&device, &config);
-        
-        let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-            label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
-        });
-        
-        let render_pipeline_layout = 
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
-                push_constant_ranges: &[],
-            });
-
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: "vs_main", 
-                buffers: &[],
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &shader,
-                entry_point: "fs_main",
-                targets: &[wgpu::ColorTargetState {
-                    format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
-                    write_mask: wgpu::ColorWrites::ALL,
-                }],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-                polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLAMPING
-                clamp_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
-                conservative: false,
-            },
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            },
-        });
 
         Self {
             surface,
@@ -107,8 +56,6 @@ impl State {
             queue,
             config,
             size,
-            render_pipeline,
-            clear_color: wgpu::Color { r: 0., g: 0., b: 0., a: 1. }
         }
     }
 
@@ -122,17 +69,7 @@ impl State {
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
-        if let WindowEvent::CursorMoved { position, .. } = event {
-            self.clear_color = wgpu::Color { 
-                r: position.x / self.size.width as f64,
-                g: position.y / self.size.height as f64,
-                b: (position.x + position.y) / (self.size.width + self.size.height) as f64,
-                a: 1.0
-            };
-            true
-        } else {
-            false
-        }
+        false
     }
 
     fn update(&mut self) {
@@ -148,21 +85,23 @@ impl State {
         });
         
         {
-            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[wgpu::RenderPassColorAttachment {
                     view: &view,
                     resolve_target: None,
                     ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(self.clear_color),
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
                         store: true,
                     },
                 }],
                 depth_stencil_attachment: None,
             });
-
-            render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.draw(0..3, 0..1);
         }
         
         // submit will accept anything that implements IntoIter
@@ -183,7 +122,7 @@ fn main() {
     event_loop.run(move |event, _, control_flow| 
         match event {
             Event::WindowEvent { ref event, window_id, } 
-            if window_id == window.id() && !state.input(event) => 
+            if window_id == window.id() || !state.input(event) => 
                 match event {
                     WindowEvent::CloseRequested | WindowEvent::KeyboardInput {
                         input:
