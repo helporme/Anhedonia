766512aa2c3bac05873068a8b0669a0c3942c935
Daniel <helporme.dev@gmail.com>
2022-12-09 02:39:35 +0100

    Refactor integration tests

--- crates/graph-frame/tests/test_graph_implicit_behaviour.rs
+++ crates/graph-frame/tests/test_graph_implicit_behaviour.rs
@@ -1,95 +1,38 @@
+#[macro_use]
+mod utils;
+
 use std::sync::RwLock;
-
-use graph_frame::builder::*;
+use graph_frame::building::*;
 use graph_frame::resource::any_storage::AnyStorage;
-use graph_frame::resource::{Mut, Ref, Lock};
+use graph_frame::resource::{Mut, Ref};
 
-use macros::composed;
+impl_resources!(A, B, C, D, E, F, I, O);
+
+type Order = RwLock<Vec<&'static str>>;
 
 #[test]
 fn node_exec_only_when_res_available() {
-    fn system0(_: Ref<u8>) {
-        unreachable!()
-    }
+    let mut graph = build_graph!(system_0, system_1);
 
-    fn system1(lock: Lock<u16>) {
-        *lock.write() += 1;
-    }
+    let order = Order::default();
+    add_resources!(graph, order, I, B);
 
-    let mut graph = GarphBuilder::new()
-        .with_system(system0)
-        .with_system(system1)
-        .with_kit(composed!(any_storage: AnyStorage => AnyStorage::new()))
-        .build();
+    graph.execute();
 
-    graph.kit_mut().any_storage.insert(0u16);
-
-    for _ in 0..3 {
-        graph.execute();
-    }
+    let order = graph.kit_mut().any_storage.remove::<RwLock<Vec<&'static str>>>()
+        .unwrap()
+        .into_inner()
+        .unwrap();
+    
+    assert_eq!(order, vec!["system_1"]);
 }
 
 #[test]
 fn correct_sorted_node_exec_order() {
-    //        
-    //  /-> s0 -> A -> s2 -> F -\
-    // I       \                 > s5 -> O
-    //  \-> s1 -> B -> s3 -> E -/
-    //            \--> s4 -> D /
+    let mut graph = build_graph!(system_2, system_3, system_5, system_4, system_0, system_1);
 
-    struct A; struct B; struct C;
-    struct D; struct E; struct F;
-    struct I; struct O;
-
-    fn system_0(i: Ref<I>, a: Mut<A>, b: Mut<B>, 
-        order: Ref<RwLock<Vec<&'static str>>>) {
-        order.write().unwrap().push("system_0");
-    }
-
-    fn system_1(i: Ref<I>, b: Mut<B>,
-        order: Ref<RwLock<Vec<&'static str>>>) {
-        order.write().unwrap().push("system_1");
-    }
-
-    fn system_2(a: Ref<A>, f: Mut<F>, 
-        order: Ref<RwLock<Vec<&'static str>>>) {
-        order.write().unwrap().push("system_2");
-    }
-
-    fn system_3(a: Ref<A>, b: Ref<B>, e: Mut<E>, 
-        order: Ref<RwLock<Vec<&'static str>>>) {
-        order.write().unwrap().push("system_3");
-    }
-
-    fn system_4(b: Ref<B>, d: Mut<D>, 
-        order: Ref<RwLock<Vec<&'static str>>>) {
-        order.write().unwrap().push("system_4");
-    }
-
-    fn system_5(f: Ref<F>, e: Ref<E>, d: Ref<D>, r: Mut<O>, 
-        order: Ref<RwLock<Vec<&'static str>>>) {
-        order.write().unwrap().push("system_5");
-    }
-
-    let mut graph = GarphBuilder::new()
-        .with_system(system_2)
-        .with_system(system_3)
-        .with_system(system_5)
-        .with_system(system_4)
-        .with_system(system_0)
-        .with_system(system_1)
-        .with_kit(composed!(any_storage: AnyStorage => AnyStorage::new()))
-        .build();
-
-    graph.kit_mut().any_storage.insert(RwLock::new(Vec::<&'static str>::new()));
-    graph.kit_mut().any_storage.insert(A);
-    graph.kit_mut().any_storage.insert(B);
-    graph.kit_mut().any_storage.insert(C);
-    graph.kit_mut().any_storage.insert(D);
-    graph.kit_mut().any_storage.insert(E);
-    graph.kit_mut().any_storage.insert(F);
-    graph.kit_mut().any_storage.insert(I);
-    graph.kit_mut().any_storage.insert(O);
+    let order = Order::default();
+    add_resources!(graph, order, A, B, C, D, E, F, I, O);
 
     graph.execute();
 
@@ -99,4 +42,40 @@ fn correct_sorted_node_exec_order() {
         .unwrap();
     
     assert_eq!(order, (0..6).map(|i| format!("system_{}", i)).collect::<Vec<_>>());
+}
+
+#[allow(unused_variables)]
+fn system_0(i: Ref<I>, a: Mut<A>, b: Mut<B>, 
+    order: Ref<RwLock<Vec<&'static str>>>) {
+    order.write().unwrap().push("system_0");
+}
+
+#[allow(unused_variables)]
+fn system_1(i: Ref<I>, b: Mut<B>,
+    order: Ref<RwLock<Vec<&'static str>>>) {
+    order.write().unwrap().push("system_1");
+}
+
+#[allow(unused_variables)]
+fn system_2(a: Ref<A>, f: Mut<F>, 
+    order: Ref<RwLock<Vec<&'static str>>>) {
+    order.write().unwrap().push("system_2");
+}
+
+#[allow(unused_variables)]
+fn system_3(a: Ref<A>, b: Ref<B>, e: Mut<E>, 
+    order: Ref<RwLock<Vec<&'static str>>>) {
+    order.write().unwrap().push("system_3");
+}
+
+#[allow(unused_variables)]
+fn system_4(b: Ref<B>, d: Mut<D>, 
+    order: Ref<RwLock<Vec<&'static str>>>) {
+    order.write().unwrap().push("system_4");
+}
+
+#[allow(unused_variables)]
+fn system_5(f: Ref<F>, e: Ref<E>, d: Ref<D>, r: Mut<O>, 
+    order: Ref<RwLock<Vec<&'static str>>>) {
+    order.write().unwrap().push("system_5");
 }
---
+++ crates/graph-frame/tests/utils/mod.rs
@@ -0,0 +1,24 @@
+
+// Add resources to the graph
+macro_rules! add_resources {
+    ($graph:ident, $($val:ident),*) => {
+        $($graph.kit_mut().any_storage.insert($val);)*
+    };
+}
+
+// Implement a unit struct for each identifier
+macro_rules! impl_resources {
+    ($($val:ident),*) => {
+        $(struct $val;)*
+    };
+}
+
+// Build the graph with systems
+macro_rules! build_graph {
+    ($($system:ident),*) => {
+        GarphBuilder::new()
+            $(.with_system($system))*
+            .with_kit(macros::composed!(any_storage: AnyStorage => AnyStorage::new()))
+            .build()
+    };
+}
